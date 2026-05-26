028dd40c47ff29d685560e9bf1f9efed553c3a41
Sanvi Rastogi <rastogi.s2006@gmail.com>
2026-05-26 21:04:17 +0530

    db.js updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,20 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGO_URI || "mongodb://localhost:27017/transport_platform",
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      family: 4,
+      serverSelectionTimeoutMS: 30000,
+    });
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`DB Error: ${error.message}`);
+    console.error("FULL DB ERROR:");
+    console.error(error);
+
     process.exit(1);
   }
 };
